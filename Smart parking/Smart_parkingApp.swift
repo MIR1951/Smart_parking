e6b2a3545a3b946e990b70dfc5f96eff01d39c21
MIR1951 <160203220+MIR1951@users.noreply.github.com>
2025-11-30 03:41:01 +0500

    Initial Commit

---
+++ Smart parking/Smart_parkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_parkingApp.swift
+//  Smart parking
+//
+//  Created by Kenjaboy Xajiyev on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_parkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart parkingTests/Smart_parkingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_parkingTests.swift
+//  Smart parkingTests
+//
+//  Created by Kenjaboy Xajiyev on 30/11/25.
+//
+
+import Testing
+@testable import Smart_parking
+
+struct Smart_parkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
